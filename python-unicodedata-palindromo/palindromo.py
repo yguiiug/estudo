833e0aee311c2997b5337f89409d4ac1748c8a72
Guilherme <yguiiug@gmail.com>
2025-05-12 14:15:27 -0300

    Atualiza palindromo

--- python-unicodedata-palindromo/palindromo.py
+++ python-unicodedata-palindromo/palindromo.py
@@ -1,13 +1,15 @@
 import unicodedata
 import string
+
 palavra = input("Digite uma frase: ")
+
 frase = palavra.lower()
-for caractere in [" ", ",", ".", "-"]:
-    frase = frase.replace(caractere, "")
-frase = "".join([letra for letra in frase if letra in string.ascii_lowercase + string.digits])
 frase = unicodedata.normalize("NFD", frase)
 frase = "".join([letra for letra in frase if unicodedata.category(letra) != "Mn"])
+frase = "".join([letra for letra in frase if letra in string.ascii_lowercase])
+
 cont = frase[::-1]
+
 if cont == frase:
     print(f"{palavra} É um palíndromo")
 else:
